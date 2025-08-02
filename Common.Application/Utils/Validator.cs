24dd9f4c6d292ca4747da7f740022dfb97c91e8b
RubenFern <rubenferval@hotmail.com>
2025-08-03 00:49:48 +0200

    added class validator

---
+++ Common.Application/Utils/Validator.cs
@@ -0,0 +1,28 @@
+﻿namespace Common.Application.Utils;
+
+public static class Validator
+{
+    public static void CheckGuid(Guid code)
+    {
+        if (code == Guid.Empty)
+            throw new ArgumentNullException(nameof(code));
+    }
+    
+    public static void CheckNullArgument(string? argument, string field)
+    {
+        if (string.IsNullOrEmpty(argument))
+            throw new ArgumentNullException(field);
+    }
+
+    public static void CheckPositiveValue(double value, string field)
+    {
+        if (value < 0)
+            throw new ArgumentOutOfRangeException(field);
+    }
+
+    public static void CheckPositiveValue(int value, string field)
+    {
+        if (value < 0)
+            throw new ArgumentOutOfRangeException(field);
+    }
+}
