e1bcb0ae42d89b2b821bb0729791d7d9281c781c
RubenFern <rubenferval@hotmail.com>
2025-09-08 02:32:42 +0200

    refactor

--- Common.Application/Services/ImageService.cs
+++ Common.Application/Services/ImageService.cs
@@ -1,6 +1,7 @@
 ﻿using CloudinaryDotNet;
 using CloudinaryDotNet.Actions;
 using Common.Application.Interfaces;
+using Common.Application.Utils;
 using Newtonsoft.Json.Linq;
 
 namespace Common.Application.Services;
@@ -19,10 +20,9 @@ public class ImageService : IImageService
 
     public async Task<string> UploadImage(FileEntityDto fileEntityDto)
     {
-        if (fileEntityDto.Length == 0)
-            throw new ArgumentException("FileEntityDto.Length is zero");
+        CheckImageFile(fileEntityDto);        
 
-        fileEntityDto.FileName = Guid.NewGuid() + Path.GetExtension(fileEntityDto.FileName);
+        fileEntityDto.FileName = Guid.NewGuid() + GetExtensionFile(fileEntityDto);
         
         await using Stream stream = new MemoryStream(fileEntityDto.Content);
 
@@ -37,6 +37,24 @@ public class ImageService : IImageService
         JToken jsonResult = (await _cloudinary.UploadAsync(uploadParams)).JsonObj;
 
         return jsonResult["secure_url"]?.ToString() ?? throw new InvalidOperationException("secure_url not exists");
+    }
+
+    private void CheckImageFile(FileEntityDto fileEntityDto)
+    {
+        if (fileEntityDto.Length == 0)
+            throw new ArgumentException("FileEntityDto.Length is zero");
+        
+        if (!Validator.ValidExtensionsImage.Contains(GetExtensionFile(fileEntityDto)))
+            throw new ArgumentException("Invalid file extension");
+        
+        if (fileEntityDto.ContentType is null || !Validator.ValidContentTypeImage.Contains(fileEntityDto.ContentType))
+            throw new ArgumentException("Invalid file type");
+    }
+
+    private string GetExtensionFile(FileEntityDto fileEntityDto)
+    {
+        return Path.GetExtension(fileEntityDto.FileName) 
+               ?? throw new ArgumentException("FileName is not a file");
     }
     
 }
